21c094fec52aef4e67390c005e4ad5d81604ef15
Emily Lewis <emilymlewis0@gmail.com>
2024-08-08 14:40:00 -0400

    Add pages with basic content

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,11 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "@/layouts";
+import { AboutPage, ProjectsPage, StartPage } from "@/pages";
 
 export const AppRouter: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route index element={<></>} />
+        <Route index element={<StartPage/>} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,5 +1,11 @@
 import { Typography } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 export const DefaultLayout: React.FC = () => {
-  return <Typography>content</Typography>;
+  return (
+    <>
+      <Typography>Default layout</Typography>
+      <Outlet />
+    </>
+  );
 };
