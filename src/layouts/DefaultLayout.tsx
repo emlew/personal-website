028607bc90818bab3076912c2160a7eba11df5b8
Emily Lewis <emilymlewis0@gmail.com>
2024-08-08 22:09:33 -0400

    Add NavBar

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,10 +1,10 @@
-import { Typography } from "@mui/material";
+import { NavBar } from "@/components";
 import { Outlet } from "react-router-dom";
 
 export const DefaultLayout: React.FC = () => {
   return (
     <>
-      <Typography>Default layout</Typography>
+      <NavBar />
       <Outlet />
     </>
   );
